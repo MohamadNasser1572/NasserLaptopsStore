ccff319fe50e6efad21c554bf2f68453f39aa122
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-17 17:05:58 +0200

    Initial commit in main

---
+++ src/main/java/com/nasser/NasserLaptopsStore/NasserLaptopsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.nasser.NasserLaptopsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasserLaptopsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasserLaptopsStoreApplication.class, args);
+	}
+
+}
