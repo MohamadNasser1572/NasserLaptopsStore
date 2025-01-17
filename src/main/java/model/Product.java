837ec57a8d7022852c0b94a2b0f63b54927e6b62
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-17 17:58:58 +0200

    feat: add product model

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private Date createdAt;
+
+}
