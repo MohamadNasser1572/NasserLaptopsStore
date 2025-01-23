7daa265b13d60e90c9a22d99f552b9cbab85bd66
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-24 00:53:34 +0200

    feat: add dto and the commit before is fix; update service from ntity to dto

---
+++ src/main/java/com/nasser/NasserLaptopsStore/dto/ProductDto.java
@@ -0,0 +1,14 @@
+package com.nasser.NasserLaptopsStore.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDto {
+
+    private String name;
+    private String brand;
+    private String category;
+    private double price;
+    private String description;
+
+}
