0ee97d673eba9ce6001d0a3579fa85bda14719e0
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-01-24 00:51:46 +0200

    feat; add dto for data transfer between product entity and values

--- src/main/java/com/nasser/NasserLaptopsStore/service/ProductService.java
+++ src/main/java/com/nasser/NasserLaptopsStore/service/ProductService.java
@@ -1,7 +1,8 @@
 package com.nasser.NasserLaptopsStore.service;
 
+import com.nasser.NasserLaptopsStore.dto.ProductDto;
 import com.nasser.NasserLaptopsStore.model.Product;
 
 public interface ProductService {
-Product addProduct(Product product);
+Product createProduct(ProductDto productDto);
 }
